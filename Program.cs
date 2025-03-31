6fd7aa2a481f5b19434478a178aeb3811671c33f
Sam Dicken <9944905+ProfessorDudley@users.noreply.github.com>
2025-03-31 16:25:00 +0100

    moved VideGame class to separate file.

--- Program.cs
+++ Program.cs
@@ -1,102 +1,81 @@
 ﻿using GameLibraryTracker;
 
 class VideoGameTracker
+{
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
+
+    Console.WriteLine("Welcome to the Video Game Tracker!");
+
+    
+
+    while (true)
     {
+      Console.Write(">> ");
+      string command = (Console.ReadLine() ?? string.Empty).ToLower();
+      if (command == "exit") break; // allows the program to exit.
+      string? filter;
+      // all other instructions are processed:
+      switch (command.Split(" ")[0])
+      {
+        case "add": // Adds a game to the library.
+          GameLibrary.AddGame();
+          break;
+        case "save": // Saves game library to list.
+          break;
+        case "load": // Loads library from disk
+          break;
+        case "list": // Lists all games in library.
+          foreach (VideoGame game in GameLibrary.Library)
+          {
+            Console.WriteLine(game.Alias);
+          }
+          break;
+        case "filter":
+          try
+          {
+            filter = command.Split(" ")[1];
+          }
+          catch (IndexOutOfRangeException)
+          {
+            Console.WriteLine("Error: Unable to parse command.");
+            Console.WriteLine("Usage: 'filter {platform}'");
+            break;
+          }
+          GameLibrary.ListGames(filter);
+          break;
+        case "remove":
+          try
+          {
+            filter = command.Split(" ")[1];
+          }
+          catch (IndexOutOfRangeException)
+          {
+            Console.WriteLine("Error: Unable to parse command.");
+            Console.WriteLine("Usage: 'remove {game}'");
+            break;
+          }
+          GameLibrary.RemoveGame(filter);
+          break;
 
-      Console.WriteLine("Welcome to the Video Game Tracker!");
-
-      // Create a path to the library.
-      string path = Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/videogametracker/").FullName;
-      string libPath;
-      if (!File.Exists(path + "data.json"))
-      {
-        using (var stream = File.Create(path + "data.json"))
-        {
-          libPath = stream.Name;
-        }
-      } else
-      {
-        libPath = path + "data.json";
+        case "count": // Outputs the number of games in the library.
+          Console.WriteLine($"{GameLibrary.Library.Count} games in library");
+          break;
+        case "help":
+        // continue to default
+        default:
+          // exits switch statement if no command is matched.
+          Console.WriteLine("Command List:");
+          Console.WriteLine("help:    Show this help screen.");
+          Console.WriteLine("add:     Adds a game to the library. Does not save to disk.");
+          Console.WriteLine("filter:  Filters the library for a given platform.");
+          Console.WriteLine("load:    Loads library from disk.");
+          Console.WriteLine("save:    Saves library to disk");
+          Console.WriteLine("exit:    Exits the program.");
+          break;
       }
-      
-      
-      // Load library
-      GameLibrary.LoadGames(libPath);
-      
-      while (true)
-      {
-        Console.Write(">> ");
-        string command = (Console.ReadLine() ?? string.Empty).ToLower();
-        if (command == "exit") break; // allows the program to exit.
-        string? filter;
-        // all other instructions are processed:
-        switch (command.Split(" ")[0])
-        {
-          case "add": // Adds a game to the library.
-            GameLibrary.AddGame();
-            break;
-          case "save": // Saves game library to list.
-            GameLibrary.WriteOut(libPath);
-            Console.WriteLine("Save complete!");
-            break;
-          case "load": // Loads library from disk
-            GameLibrary.LoadGames(libPath);
-            foreach (VideoGame game in GameLibrary.Library)
-            {
-              Console.WriteLine($"Loaded {game.Alias} for {game.Platform}.");
-            }
-            break;
-          case "list": // Lists all games in library.
-              foreach (VideoGame game in GameLibrary.Library)
-              {
-                Console.WriteLine(game.Alias);
-              }
-            break;
-          case "filter":
-            try
-            {
-              filter = command.Split(" ")[1];
-            } catch (IndexOutOfRangeException)
-            {
-              Console.WriteLine("Error: Unable to parse command.");
-              Console.WriteLine("Usage: 'filter {platform}'");
-              break;
-            }
-            GameLibrary.ListGames(filter);
-            break;
-          case "remove":
-            try
-            {
-              filter = command.Split(" ")[1];
-            } catch (IndexOutOfRangeException)
-            {
-              Console.WriteLine("Error: Unable to parse command.");
-              Console.WriteLine("Usage: 'remove {game}'");
-              break;
-            }
-            GameLibrary.RemoveGame(filter);
-            break;
-                        
-          case "count": // Outputs the number of games in the library.
-            Console.WriteLine($"{GameLibrary.Library.Count} games in library");
-            break;
-          case "help":
-            // continue to default
-          default:
-            // exits switch statement if no command is matched.
-            Console.WriteLine("Command List:");
-            Console.WriteLine("help:    Show this help screen.");
-            Console.WriteLine("add:     Adds a game to the library. Does not save to disk.");
-            Console.WriteLine("filter:  Filters the library for a given platform.");
-            Console.WriteLine("load:    Loads library from disk.");
-            Console.WriteLine("save:    Saves library to disk");
-            Console.WriteLine("exit:    Exits the program.");
-            break;
-        }
-      }
-      
-      
     }
+
+
   }
+}
